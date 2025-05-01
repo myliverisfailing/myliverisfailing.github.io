317e1af6964dc1869414fba04be5843d4636b88b
myliverisfailing <rileyclement+school@student.net.nz>
2025-05-02 02:10:23 +1200

    reuploaded src directory

---
+++ src/script.js
@@ -0,0 +1,14 @@
+/*
+This code animates the header text appear one letter at a time every 50ms (milliseconds)
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = "New Zealand Native Birds.";
+    const header = document.getElementById('header');
+    
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(function() {
+            header.innerHTML += text.charAt(i);
+        }, 50 * i);
+    }
+});
